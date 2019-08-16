3f00d54cb9dec7c62488c382868ff5f7e3589aa4
hanbing <beatmight@gmail.com>
2019-08-16 14:52:20 +0800

    增加了测试用例

--- tests/test_v2.py
+++ tests/test_v2.py
@@ -1,3 +1,4 @@
+import os
 from src import diagnosis_v2
 
 def test_chest_diagnosis_v2(dicom_test_files):
@@ -6,3 +7,18 @@ def test_chest_diagnosis_v2(dicom_test_files):
         h, figure = diagnosis_v2(f)
 
 
+def test_batch(src_dest_mapping):
+	
+	error_list = []
+
+	for src, target in src_dest_mapping.items():
+		try:
+			h, figure = diagnosis_v2(src)
+		except:
+			error_list.append(src)
+			continue
+
+		if not os.path.isdir(os.path.dirname(target)):
+			os.makedirs(os.path.dirname(target))
+
+		figure.save(target)
