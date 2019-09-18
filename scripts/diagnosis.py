b0f20726d95e531c8bdda07027ec44ebcc1d2e25
hanbing <beatmight@gmail.com>
2019-09-18 09:59:38 +0800

    完善了诊断脚本

--- scripts/diagnosis.py
+++ scripts/diagnosis.py
@@ -7,15 +7,26 @@ import pydicom
 import cv2
 import argparse
 
-from src import diagnosis_v2
+import numpy as np
+
+from src import diagnosis_v2, depression_degree
 
 parser = argparse.ArgumentParser()
-parser.add_argument("src_dir", type=str, required=True, help="病人胸部ct文件夹路径。")
-parser.add_argument("dest_dir", type=str, required=True, help="结果输出目录。")
+parser.add_argument("src_dir", type=str, help="病人胸部ct文件夹路径。")
+parser.add_argument("dest_dir", type=str, help="结果输出目录。")
 
 args = parser.parse_args()
 
 all_file = glob.glob(os.path.join(args.src_dir, "FILE*"))
 
-for i, f in enumerate(all_file):
-    pass
+degrees = np.array([depression_degree(i) for i in all_file])
+index = np.argmax(degrees)
+file_name = all_file[index]
+
+haller_index, figure = diagnosis_v2(file_name)
+
+figure.save(os.path.join(args.dest_dir, "result.png"))
+with open(os.path.join(args.dest_dir, "haller.txt"), 'w') as f:
+    f.write("Haller 指数值： %f" % haller_index)
+
+    
