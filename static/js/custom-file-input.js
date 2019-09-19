2fac6bb0d6ee6340420bb7a0cabc3082048fbfe3
hanbing <beatmight@gmail.com>
2019-09-19 14:59:59 +0800

    post 服务器地址

--- static/js/custom-file-input.js
+++ static/js/custom-file-input.js
@@ -11,7 +11,7 @@ function postData(files){
 		formData.append('file['+i+']', file);
 	});
     $.ajax({
-        url:'http://127.0.0.1:10001/yuyi/api/chest/v2', /*接口域名地址*/
+        url:'http://183.214.140.56:10001/yuyi/api/chest/v2', /*接口域名地址*/
         type:'post',
         data: formData,
         contentType: false,
