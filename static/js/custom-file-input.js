3f2c81f345b9f3fc50950dfc1b41c10bcfadc2e4
hanbing <beatmight@gmail.com>
2019-10-31 14:43:25 +0800

    张岩添加了上传 进度条

--- static/js/custom-file-input.js
+++ static/js/custom-file-input.js
@@ -10,12 +10,28 @@ function postData(files){
 	$.each(files,function(i,file){
 		formData.append('file['+i+']', file);
 	});
+	//侦查附件上传情况 ,这个方法大概0.05-0.1秒执行一次
+	function OnProgRess(event) {
+		var event = event || window.event;
+		//console.log(event);  //事件对象
+		console.log("已经上传：" + event.loaded); //已经上传大小情况(已上传大小，上传完毕后就 等于 附件总大小)
+		//console.log(event.total);  //附件总大小(固定不变)
+		var loaded = Math.floor(100 * (event.loaded / event.total)); //已经上传的百分比  
+		$("#speed").html(loaded + "%").css("width", loaded + "%");
+	};
+	var xhr = $.ajaxSettings.xhr(); //创建并返回XMLHttpRequest对象的回调函数(jQuery中$.ajax中的方法)	
   $.ajax({
     url:'http://183.214.140.56:10001/yuyi/api/chest/v2', /*接口域名地址*/
     type:'post',
     data: formData,
     contentType: false,
     processData: false,
+	xhr: function() {
+		if(OnProgRess && xhr.upload) {
+			xhr.upload.addEventListener("progress", OnProgRess, false);
+			return xhr;
+		}
+	}, 	
     success:function(res){
       if(res["result"]=="ok"){
       	$(".box").attr("hidden","hidden").siblings().removeAttr("hidden");
