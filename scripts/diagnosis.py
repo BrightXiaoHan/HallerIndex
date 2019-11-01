54546b00b3488bad801ce7003c609e9fe9e404f8
hanbing <beatmight@gmail.com>
2019-11-01 09:39:16 +0800

    使用统一的接口诊断文件夹

--- scripts/diagnosis.py
+++ scripts/diagnosis.py
@@ -9,7 +9,7 @@ import argparse
 
 import numpy as np
 
-from src import diagnosis_v2, depression_degree, is_avaliable
+from src import diagnosis_folder
 
 parser = argparse.ArgumentParser()
 parser.add_argument("src_dir", type=str, help="病人胸部ct文件夹路径。")
@@ -17,23 +17,12 @@ parser.add_argument("dest_dir", type=str, help="结果输出目录。")
 
 args = parser.parse_args()
 
-all_file = glob.glob(os.path.join(args.src_dir, "FILE*"))
-
-# 过滤不符合条件的照片
-all_file = list(filter(is_avaliable, all_file))
-
-# 找到凹陷最深的图片
-degrees = np.array([depression_degree(i) for i in all_file])
-index = np.argmax(degrees)
-file_name = all_file[index]
-print("最凹陷的胸片文件名: %s" % file_name)
-
 # 计算Haller指数，画辅助线
-haller_index, figure = diagnosis_v2(file_name)
+figure, haller_index = diagnosis_folder(args.src_dir, top=1)
 
 # 输出计算结果
-figure.save(os.path.join(args.dest_dir, "result.png"))
+figure[0].save(os.path.join(args.dest_dir, "result.png"))
 with open(os.path.join(args.dest_dir, "haller.txt"), 'w') as f:
-    f.write("Haller 指数值： %f" % haller_index)
+    f.write("Haller 指数值： %f" % haller_index[0])
 
     
