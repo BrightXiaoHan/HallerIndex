ca6eb643e7bb72c0e52484559f750cf3afd6a8f3
wuyubo <844529478@qq.com>
2020-01-02 10:39:26 +0800

    计算凹陷程度前先旋转至水平

--- scripts/diagnosis_batch.py
+++ scripts/diagnosis_batch.py
@@ -26,17 +26,20 @@ for folder in tqdm(list(os.walk(args.src_dir))[0][1]):
         shutil.rmtree(os.path.join(args.dest_dir, folder))
     os.makedirs(os.path.join(args.dest_dir, folder))
 
-    path = os.path.join(os.path.join(args.src_dir, folder), "FILE*")
-    all_file = glob.glob(path)
-    for f in all_file:
-        # 读取dicom文件中的像素数据
-        try:
-            ds = pydicom.dcmread(f)
-        except pydicom.errors.InvalidDicomError as e:
-            continue
-
-        saved_path = os.path.join(os.path.join(args.dest_dir, folder), os.path.basename(f) + ".png")
-        pylab.imsave(saved_path, get_default_image(ds), cmap=pylab.cm.gray)
+    # path = os.path.join(os.path.join(args.src_dir, folder), "FILE*")
+    # all_file = glob.glob(path)
+    # for f in all_file:
+    #     # 读取dicom文件中的像素数据
+    #     try:
+    #         ds = pydicom.dcmread(f)
+    #     except pydicom.errors.InvalidDicomError as e:
+    #         continue
+    #
+    #     saved_path = os.path.join(os.path.join(args.dest_dir, folder), os.path.basename(f) + ".png")
+    #     try:
+    #         pylab.imsave(saved_path, get_default_image(ds), cmap=pylab.cm.gray)
+    #     except:
+    #         continue
 
     try:
         figures, indexes, fnames = diagnosis_folder(os.path.join(args.src_dir, folder), _return_files=True, _debug=True)
